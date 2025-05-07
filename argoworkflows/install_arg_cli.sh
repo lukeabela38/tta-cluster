06d2af8c447be5addefc58502663a06566a36344
luke <lukeabela56@gmail.com>
2025-05-07 10:50:51 +0200

    Lots of new resources

---
+++ argoworkflows/install_arg_cli.sh
@@ -0,0 +1,20 @@
+# Detect OS
+ARGO_OS="darwin"
+if [[ uname -s != "Darwin" ]]; then
+  ARGO_OS="linux"
+fi
+
+# Download the binary
+curl -sLO "https://github.com/argoproj/argo-workflows/releases/download/v3.6.7/argo-$ARGO_OS-arm64.gz"
+
+# Unzip
+gunzip "argo-$ARGO_OS-arm64.gz"
+
+# Make binary executable
+chmod +x "argo-$ARGO_OS-arm64"
+
+# Move binary to path
+mv "./argo-$ARGO_OS-arm64" /usr/local/bin/argo
+
+# Test installation
+argo version
